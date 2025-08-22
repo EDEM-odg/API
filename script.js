635cc206705244e4196c33bdd450703d3b239810
EDEM-odg <amenakporjudecartel@gmail.com>
2025-08-22 10:17:42 +0000

    Added new task

--- script.js
+++ script.js
@@ -8,59 +8,31 @@ const form = document.getElementById("form");
 const search = document.getElementById("query")
 
 returnMovies(APILINK)
-function returnMovies(url){data.results.forEach(element => {
-    const div_card = document.createElement('div');
-    div_card.setAttribute('class', 'card');
-
-    const div_row = document.createElement('div');
-    div_row.setAttribute('class', 'row');
-
-    const div_column = document.createElement('div');
-    div_column.setAttribute('class', 'column');
-
-    const image = document.createElement('img');
-    image.setAttribute('class', 'thumbnail');
-    image.setAttribute('id', 'image');
-
-    if (element.poster_path) {
-      image.src = IMG_PATH + element.poster_path;
-    } else {
-      image.src = `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}`;
-    }
-
-    const center = document.createElement('center');
-    center.appendChild(image);
-
-    div_card.appendChild(center);
-    div_column.appendChild(div_card);
-    div_row.appendChild(div_column);
-    main.appendChild(div_row);
-  });
-
+function returnMovies(url) {
   fetch(url).then(res => res.json())
-  .then(function(data){
+  .then(function(data)){
     console.log(data.results);
     data.results.forEach(element => {
       const div_card = document.createElement('div');
-      div_card.setAttribute('class', 'card');
+      div_card.setAttribute('class','card');
 
       const div_row = document.createElement('div');
-      div_row.setAttribute('class', 'row');
+      div_row.setAttribute('class','row');
 
       const div_column = document.createElement('div');
-      div_column.setAttribute('class', 'column');
+      div_column.setAttribute('class','column');
 
       const image = document.createElement('img');
-      div_card.setAttribute('class', 'thumbnail');
-      div_card.setAttribute('id', 'image');
-
+      image.setAttribute('class','thumbnail');
+      image.setAttribute('id','image');
 
       const title = document.createElement('h3');
-       div_card.setAttribute('id', 'title');
+      title.setAttribute('id','title');
 
       const center = document.createElement('center');
 
       title.innerHTML = `${element.title}`;
+      image.src = IMG_PATH + element.poster_path;
 
       center.appendChild(image);
       div_card.appendChild(center);
@@ -69,8 +41,8 @@ function returnMovies(url){data.results.forEach(element => {
       div_row.appendChild(div_column);
 
       main.appendChild(div_row);
-      });
-  });
+    }
+  }
 }
 
 form.addEventListener("submit",(e) =>{
@@ -84,6 +56,4 @@ form.addEventListener("submit",(e) =>{
     search.value = "";
   }
 });
-
-
   
